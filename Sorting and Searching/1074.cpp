872c471803fb41987781a9533a552d3e384ff0b7
freakin23 <rameezparwez@gmail.com>
2024-03-31 13:54:42 +0530

    Initial Commit

---
+++ Sorting and Searching/1074.cpp
@@ -0,0 +1,31 @@
+// Problem Link: https://cses.fi/problemset/task/1074
+
+#include <bits/stdc++.h>
+#define i23 long long
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<int> Arr(n);
+    for (auto &x : Arr) {
+        std::cin >> x;
+    }
+    std::sort(begin(Arr), end(Arr));
+    int median = Arr[n / 2];
+    i23 res = 0;
+
+    for (auto x : Arr) {
+        res += abs(median - x);
+    }
+    std::cout << res << '\n';
+
+}
+int main() {
+    // int t;
+    // std::cin >> t;
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ Sorting and Searching/1629.cpp
@@ -0,0 +1,37 @@
+// Problem Link: https://cses.fi/problemset/task/1629
+
+#include <bits/stdc++.h>
+ 
+void solve() {
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<int, int>> Arr(n);
+    for (int i = 0; i < n; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        Arr[i] = {a, b};
+    }
+    std::sort(begin(Arr), end(Arr), [&] (auto &i, auto &j) {
+        return i.second < j.second;
+    });
+ 
+    int res = 1;
+    int prev = Arr[0].second;
+    for (int i = 1; i < n; i++) {
+        if (Arr[i].first >= prev) {
+            prev = Arr[i].second;
+            res += 1;
+        }
+    }
+    std::cout << res << '\n';
+ 
+}
+int main() {
+    // int t;
+    // std::cin >> t;
+    int t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
