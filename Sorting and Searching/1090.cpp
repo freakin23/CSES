5ba6cb68e208781d7f519a298d9a1aecea0a1bd8
freakin23 <rameezparwez@gmail.com>
2024-04-16 20:47:54 +0530

    Added New Solution

--- Sorting and Searching/1090.cpp
+++ Sorting and Searching/1090.cpp
@@ -3,40 +3,30 @@
 #include <bits/stdc++.h>
 #define i23 long long
 
-void solve() {
-    int n, x;
-    std::cin >> n >> x;
-    std::vector<int> p(n);
-    for (auto &pp : p) {
-        std::cin >> pp;
-    }
-    std::sort(begin(p), end(p));
-    std::vector<int> vis(n, 0);
-    int j = n - 1;
-    int i = 0, res = 0;
-    while (i < n) {
-        if (!vis[i]) {
-            while (j > i and p[j] + p[i] > x) {
-                j--;
-            }
-            if (j > i) {
-                res += 1;
-                vis[i] = 1;
-                vis[j] = 1;
-                j--;
-            }
-        }
-        i++;
-    }
-    res += count(begin(vis), end(vis), 0);
-    std::cout << res << '\n';
-
-}
 int main() {
-    int t = 1;
-    while (t--) {
-        solve();
+    int n, k;
+    std::cin >> n >> k;
+    std::vector<int> p(n);
+    for (auto &x : p) {
+        std::cin >> x;
     }
+    int res = 0;
+    std::sort(begin(p), end(p));
+    int i = 0, j = n - 1;
+    while (i < j) {
+        if (p[i] + p[j] > k) {
+            res += 1;
+            j--;
+        } else if (p[i] + p[j] <= k) {
+            res += 1;
+            i++;
+            j--;
+        }
+    }
+    if (i == j) {
+        res += 1;
+    }
+    std::cout << res << '\n';
     return 0;
 }
 
