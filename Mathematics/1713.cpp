df9c677cbc5af6a68b255a20e58c83629d236889
freakin23 <rameezparwez@gmail.com>
2024-08-14 13:31:18 +0530

    update 1713.cpp

---
+++ Mathematics/1713.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const int MOD = 1'000'000'007;
+
+template<typename T>
+std::vector<T> factor(T x) {
+    std::vector<T> res;
+    for (T i = 1; i * i <= x; i++) {
+        if (x % i == 0) {
+            res.push_back(i);
+            if (x / i != i) {
+                res.push_back(x / i);
+            }
+        }
+    }
+    std::sort(begin(res), end(res));
+    return res;
+}
+
+void solve() {
+    int n;
+    std::cin >> n;
+    auto f = factor(n);
+    std::cout << (int)f.size() << '\n';
+}
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin.exceptions(std::ios::badbit|std::ios::failbit);
+    int test = 1;
+    std::cin >> test;
+    while (test-- > 0) {
+        solve();
+    }
+    return 0;
+}
