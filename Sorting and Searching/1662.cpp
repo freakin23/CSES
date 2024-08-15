f97d088a76ef44fe05f96401e0ac02e73b621c8d
freakin23 <rameezparwez@gmail.com>
2024-08-15 21:51:54 +0530

    upd

---
+++ Sorting and Searching/1662.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<int> Arr(n);
+    for (auto &x : Arr) {
+        std::cin >> x;
+    }
+
+    long long res = 0;
+}
