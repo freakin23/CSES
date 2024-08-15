841589ad8e182cc002a39b780ffbea766d61dd3d
freakin23 <rameezparwez@gmail.com>
2024-08-15 21:48:04 +0530

    update 2428.cpp

---
+++ Sorting and Searching/2428.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define i23 long long
+ 
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const int MOD = 1'000'000'007;
+ 
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> Arr(n);
+    for (auto &x : Arr) {
+        cin >> x;
+    }
+    map<int, int> f;
+    i23 res = 0;
+    int c = 0;
+    for (int i = 0, j = 0; i < n; i++) {
+        while (j < n and c + (f[Arr[j]] == 0) <= k) {
+            f[Arr[j]]++;
+            c += (f[Arr[j]] == 1);
+            j++;
+        }
+        res += j - i;
+        c -= (f[Arr[i]] == 1);
+        f[Arr[i]]--;
+    }
+    cout << res << '\n';
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin.exceptions(std::ios::badbit|std::ios::failbit);
+    int test = 1;
+    // std::cin >> test;
+    while (test-- > 0) {
+        solve();
+    }
+    return 0;
+}
