1290121e92abadaa989df1d062626400e30b808c
freakin23 <rameezparwez@gmail.com>
2024-08-15 22:02:10 +0530

    upd

---
+++ Sorting and Searching/1077.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+
+#define i23 long long
+ 
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const int MOD = 1'000'000'007;
+ 
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> Arr(n);
+    for (auto &x : Arr) {
+        
+    }
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin.exceptions(std::ios::badbit|std::ios::failbit);
+    int test = 1;
+    // std::cin >> test;
+    while (test-- > 0) {
+        solve();
+    }
+    return 0;
+}
