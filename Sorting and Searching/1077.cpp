b798044ed7414e273ef64dc124eea4c53c6739b6
freakin23 <rameezparwez@gmail.com>
2024-08-15 22:08:16 +0530

    upd

--- Sorting and Searching/1077.cpp
+++ Sorting and Searching/1077.cpp
@@ -20,7 +20,34 @@ void solve() {
         cin >> x;
     }
 
-    ordered_set<pair<int, int>> iset;
+    ordered_set<pair<int, int>> oset;
+    int mid = k / 2;
+    i23 l_sum = 0, r_sum = 0;
+    for (int i = 0; i < k - 1; i++) {
+        oset.insert({Arr[i], i});
+    }
+
+    for (int i = 0; i < k - 1; i++) {
+        if (i < mid) {
+            l_sum += oset.find_by_order(i)->first;
+        } else {
+            r_sum += oset.find_by_order(i)->first; 
+        }
+    }
+
+    auto insrt = [&] (pair<int, int> p) {
+        assert(oset.size() == k - 1);
+        oset.insert(p);
+        int idx = oset.order_of_key(p);
+        if (idx < mid) {
+            int median = oset.find_by_order(mid)->first;
+            l_sum += p.first;
+            l_sum -= median;
+            r_sum += median;
+        } else {
+            r_sum += p.first;
+        }
+    };
 
 }
  
