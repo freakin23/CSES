ea1e36b059cd7fd5db3f9c5d980e793b10f9a0db
freakin23 <rameezparwez@gmail.com>
2024-08-16 20:16:33 +0530

    code rfc

--- Graph Algorithms/1672.cpp
+++ Graph Algorithms/1672.cpp
@@ -2,11 +2,7 @@
 using namespace std;
 
 #define i23 long long
- 
-const int dx[4] = {-1, 1, 0, 0};
-const int dy[4] = {0, 0, -1, 1};
-const int MOD = 1'000'000'007;
- 
+  
 void solve() {
     int n, m, q;
     cin >> n >> m >> q;
