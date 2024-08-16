2c49de0996f9b2f5062292e0ed2734667d613ff1
freakin23 <rameezparwez@gmail.com>
2024-08-16 20:07:52 +0530

    upd

---
+++ Graph Algorithms/1671.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define i23 long long
+ 
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const int MOD = 1'000'000'007;
+ 
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, int>> adj[n];
+    vector<i23> dist(n, LLONG_MAX);
+    vector<bool> vis(n, 0);
+    for (int i = 1; i <= m; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        a--, b--;
+        adj[a].push_back({b, c});
+    }
+
+    auto Dijkstra = [&] () {
+        priority_queue<pair<i23, int>> pq;
+        dist[0] = 0;
+        pq.push({0, 0}); // dist, node
+
+        while (!pq.empty()) {
+            auto f = pq.top();
+            pq.pop();
+
+            if (vis[f.second]) {
+                continue;
+            }
+            vis[f.second] = 1;
+            for (auto &[b, w] : adj[f.second]) {
+                if (dist[b] > dist[f.second] + w) {
+                    dist[b] = dist[f.second] + w;
+                    pq.push({-dist[b], b});
+                }
+            }
+        }
+    };
+
+    Dijkstra();
+    for (int i = 0; i < n; i++) {
+        cout << dist[i] << " \n"[i == n - 1];
+    }
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin.exceptions(std::ios::badbit|std::ios::failbit);
+    int test = 1;
+    // std::cin >> test;
+    while (test-- > 0) {
+        solve();
+    }
+    return 0;
+}
