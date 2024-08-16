b840f6fa615a2c7884b657629ed099f6b0878e72
freakin23 <rameezparwez@gmail.com>
2024-08-16 19:49:32 +0530

    upd

---
+++ Graph Algorithms/1192.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define i23 long long
+ 
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const int MOD = 1'000'000'007;
+ 
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> g(n, vector<char> (m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> g[i][j];
+        }
+    }
+
+    function <void(int, int)> Dfs = [&] (int a, int b) {
+        if (a < 0 or a >= n or b < 0 or b >= m or g[a][b] == '#') {
+            return;
+        }
+        g[a][b] = '#';
+        Dfs(a + 1, b);
+        Dfs(a - 1, b);
+        Dfs(a, b - 1);
+        Dfs(a, b + 1);
+    };
+
+
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (g[i][j] == '.') {
+                res += 1;
+                Dfs(i, j);
+            }
+        }
+    }
+    cout << res << '\n';
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin.exceptions(std::ios::badbit|std::ios::failbit);
+    int test = 1;
+    // std::cin >> test;
+    while (test-- > 0) {
+        solve();
+    }
+    return 0;
+}
