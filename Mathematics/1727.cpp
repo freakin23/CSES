7f75b0ae4ec2b2469745e57fe07aa4ac5832dd8c
freakin23 <rameezparwez@gmail.com>
2024-09-07 12:45:13 +0530

    code rfc

--- Mathematics/1727.cpp
+++ Mathematics/1727.cpp
@@ -1,9 +1,4 @@
 #include <bits/stdc++.h>
-#define int long long
-
-const int dx[4] = {-1, 1, 0, 0};
-const int dy[4] = {0, 0, -1, 1};
-const int MOD = 1'000'000'007;
 
 void solve() {
     int n, k;
