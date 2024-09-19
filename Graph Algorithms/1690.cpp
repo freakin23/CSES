d2bbed23cf0377b23303d2ca2df8a9f09fbbc8f9
freakin23 <rameezparwez@gmail.com>
2024-09-19 12:55:31 +0530

    update 1690.cpp

---
+++ Graph Algorithms/1690.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+const int dx[4] = {-1, 1, 0, 0};
+const int dy[4] = {0, 0, -1, 1};
+const int MOD = 1'000'000'007;
+
+void solve() {
+    int n, m;
+    std::cin >> n >> m;
+    std::vector<int> G[n + 1];
+    for (int i = 1; i <= m; i++) {
+        int a, b;
+        std::cin >> a >> b;
+        G[b].push_back(a);
+    }
+
+    std::vector<std::vector<int>> Dp((1 << 20), std::vector<int> (21, 0));
+
+    Dp[1][1] = 1;
+    for (int s = 2; s < (1 << n); s++) {
+        if ((s & (1 << (n - 1))) and s != ((1 << n) - 1)) {
+            continue;
+        }
+        for (int d = 1; d <= n; d++) {
+            if ((s & (1 << (d - 1))) == 0) {
+                continue;
+            }
+            for (int v : G[d]) {
+                if (s & (1 << (v - 1))) {
+                    Dp[s][d] += Dp[s - (1 << (d - 1))][v];
+                    Dp[s][d] %= MOD;
+                }
+            }
+        }
+    }
+
+    std::cout << Dp[(1 << n) - 1][n] % MOD << '\n';
+}
+ 
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin.exceptions(std::ios::badbit|std::ios::failbit);
+    int test = 1;
+    // std::cin >> test;
+    while (test-- > 0) {
+        solve();
+    }
+    return 0;
+}
