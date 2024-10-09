11b7662896d8fdab892a271a3f6e9bea9b2c187c
freakin23 <rameezparwez@gmail.com>
2024-10-09 19:48:38 +0530

    update 1068.cpp

--- Introductory Problems/1068.cpp
+++ Introductory Problems/1068.cpp
@@ -1,1 +1,30 @@
 // Problem Link: https://cses.fi/problemset/task/1068
+
+#include <bits/stdc++.h>
+#define int long long
+
+void solve() {
+    int n;
+    std::cin >> n;
+    std::cout << n << " \n"[n == 1];
+    while (n != 1) {
+        if (n % 2 == 0) {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        std::cout << n << " \n"[n == 1];
+    }
+}
+
+int32_t main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    std::cout.tie(0);
+    std::cin.exceptions(std::ios::badbit|std::ios::failbit);
+    int test = 1;
+    while (test-- > 0) {
+        solve();
+    }
+    return 0;
+}
