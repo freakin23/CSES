8ebc1bf127b21d32af20a8b0c6a65788ad5f8be7
Rameez Parwez <79394137+freakin23@users.noreply.github.com>
2024-10-09 21:54:23 +0530

    Update 1716.cpp

--- Mathematics/1716.cpp
+++ Mathematics/1716.cpp
@@ -10,20 +10,20 @@ long long inv[MAXN + 1];
 
 /** @return x^n modulo m in O(log p) time. */
 long long exp(long long x, long long n, long long m) {
-	x %= m;  // note: m * m must be less than 2^63 to avoid ll overflow
-	long long res = 1;
-	while (n > 0) {
-		if (n % 2 == 1) { res = res * x % m; }
-		x = x * x % m;
-		n /= 2;
-	}
-	return res;
+    x %= m;  // note: m * m must be less than 2^63 to avoid ll overflow
+    long long res = 1;
+    while (n > 0) {
+	if (n % 2 == 1) { res = res * x % m; }
+	x = x * x % m;
+	n /= 2;
+    }
+    return res;
 }
 
 /** Precomputes n! from 0 to MAXN. */
 void factorial(long long p) {
-	fac[0] = 1;
-	for (int i = 1; i <= MAXN; i++) { fac[i] = fac[i - 1] * i % p; }
+    fac[0] = 1;
+    for (int i = 1; i <= MAXN; i++) { fac[i] = fac[i - 1] * i % p; }
 }
 
 /**
@@ -31,22 +31,21 @@ void factorial(long long p) {
  * from 0 to MAXN in O(n + log p) time
  */
 void inverses(long long p) {
-	inv[MAXN] = exp(fac[MAXN], p - 2, p);
-	for (int i = MAXN; i >= 1; i--) { inv[i - 1] = inv[i] * i % p; }
+    inv[MAXN] = exp(fac[MAXN], p - 2, p);
+    for (int i = MAXN; i >= 1; i--) { inv[i - 1] = inv[i] * i % p; }
 }
 
 /** @return nCr mod p */
 long long choose(long long n, long long r, long long p) {
-	return fac[n] * inv[r] % p * inv[n - r] % p;
+    return fac[n] * inv[r] % p * inv[n - r] % p;
 }
-
 
 int main() {
     factorial(MOD);
     inverses(MOD);
     int n, m;
-	std::cin >> n >> m;
-	int res = choose(n + m - 1, m, MOD); // or choose(n + m - 1, m - 1, MOD)
-	std::cout << res << '\n';
+    std::cin >> n >> m;
+    int res = choose(n + m - 1, m, MOD); // or choose(n + m - 1, m - 1, MOD)
+    std::cout << res << '\n';
     return 0;
 }
