9dce99159bc8d4e7321d56646d4559632a870f4d
freakin23 <rameezparwez@gmail.com>
2024-10-13 20:59:03 +0530

    update 1650.cpp

---
+++ Range Queries/1650.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+int main() {
+    int n, Q;
+    std::cin >> n >> Q;
+    std::vector<int> arr(n);
+    
+    for (auto &x : arr) {
+        std::cin >> x;
+    }
+    std::vector<int> xor_arr(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+        xor_arr[i + 1] = xor_arr[i] ^ arr[i];
+    }
+
+    while (Q--) {
+        int a, b;
+        std::cin >> a >> b;
+        std::cout << (xor_arr[b] ^ xor_arr[a - 1]) << '\n';
+    }
+    return 0;
+}
